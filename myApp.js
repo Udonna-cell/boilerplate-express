87d8e375a80b84099756a5cf141f4baad87e1040
udonna-cell <orabuezestanley@gmail.com>
2023-10-14 20:32:10 +0100

    serving static assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+app.use("/public",express.static(`${__dirname}/public`))
 // console.log("Hello World")
 app.get("/", (req, res)=>{
     res.sendFile(`${__dirname}/views/index.html`)
