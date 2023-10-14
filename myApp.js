6497b201c4ea66ba9aed5964939c798828555907
udonna-cell <orabuezestanley@gmail.com>
2023-10-14 23:39:22 +0100

    serving api json data

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ app.get("/", (req, res)=>{
     res.sendFile(`${__dirname}/views/index.html`)
     // res.send("Hello Express")
 })
+app.get("/json",(req, res)=>{
+    res.json({
+        message: "Hello json"
+    })
+})
 
 
 
