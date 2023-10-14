4f981083e47a3b75cc7e030e4d3d865557b9324a
udonna-cell <orabuezestanley@gmail.com>
2023-10-14 20:01:58 +0100

    part 3 sending file response

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ let app = express();
 
 // console.log("Hello World")
 app.get("/", (req, res)=>{
-    res.send("Hello Express")
+    res.sendFile(`${__dirname}/views/index.html`)
+    // res.send("Hello Express")
 })
 
 
